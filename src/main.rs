a38ab1795918575441f2d1d15314c1bd6dd1cc12
Ahmet Ege Semiz <58528905+AegisMaze@users.noreply.github.com>
2024-01-04 01:02:41 +0100

    Day 3: hangman

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,11 @@
 use std::env;
 use std::io;
+use std::io::{BufRead, BufReader};
+use std::string::String;
+use std::fs::File;
 use rand::thread_rng;
 use rand::seq::SliceRandom;
+use rand::seq::IteratorRandom;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -9,39 +13,9 @@ fn main() {
     match args[1].as_str() {
         "calculator" => calculator(),
         "eightball" => eightball(),
+        "hangman" => hangman(),
         _ => println!("Unknown command!"),
     }
-}
-
-fn eightball() {
-    println!("Ask me a yes or no question about the future:");
-
-    let mut question = String::new();
-    io::stdin().read_line(&mut question).expect("Question could not be read");
-
-    let responses = vec![
-    "It is certain",
-    "It is decidedly so",
-    "Without a doubt",
-    "Yes definitely",
-    "You may rely on it",    
-    "As I see it, yes",
-    "Most likely",
-    "Outlook good",
-    "Yes",
-    "Signs point to yes",
-    "Reply hazy, try again",
-    "Ask again later",
-    "Better not tell you now",
-    "Cannot predict now",
-    "Concentrate and ask again",    
-    "Don't count on it",
-    "My reply is no",
-    "My sources say no",
-    "Outlook not so good",
-    "Very doubtful" 
-    ];
-    println!("{}", responses.choose(&mut thread_rng()).expect("Fate itself seems to be broken..."));
 }
 
 fn calculator() {
@@ -110,4 +84,118 @@ fn calculator() {
         }
     }
 
+}
+
+fn eightball() {
+    println!("Ask me a yes or no question about the future:");
+
+    let mut question = String::new();
+    io::stdin().read_line(&mut question).expect("Question could not be read");
+
+    let responses = vec![
+    "It is certain",
+    "It is decidedly so",
+    "Without a doubt",
+    "Yes definitely",
+    "You may rely on it",    
+    "As I see it, yes",
+    "Most likely",
+    "Outlook good",
+    "Yes",
+    "Signs point to yes",
+    "Reply hazy, try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",    
+    "Don't count on it",
+    "My reply is no",
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful" 
+    ];
+    println!("{}", responses.choose(&mut thread_rng()).expect("Fate itself seems to be broken..."));
+}
+
+fn hangman() {
+    println!("Welcome to hangman! Enter a lowercase letter to make a guess, seven wrong guesses and you lose");
+    println!("Enter exit to leave the game");
+
+    let filename = "src/hangman.txt";
+    let f = File::open(filename)
+        .unwrap_or_else(|_| panic!("File not found"));
+    let f = BufReader::new(f);
+
+    let lines = f.lines().map(|l| l.expect("Couldn't read line"));
+
+    let word = lines
+        .choose(&mut rand::thread_rng())
+        .expect("File had no lines");
+
+    let mut wrong_guesses = 0;
+    
+    let mut gallows = String::from(" _____\n |    \n |     \n |     \n |     \n |");
+    let mut corrects = String::from(" _".repeat(word.len()));
+    let mut guessed = String::from(" Wrong guesses:");
+    
+    let mut count = 0;
+
+    loop {
+        println!("{}\n\n{}\n\n{}", gallows, corrects, guessed);
+
+        if wrong_guesses == 7 {
+            println!("You lose! The word was: {}", word);
+            return;
+        }
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).expect("Input could not be read");
+
+        if input == "exit\n" {
+            break;
+        }
+
+        if input.len() != 2 {
+            println!("Please enter a single, lowercase letter as input");
+            continue;
+        }
+
+        let letter = input.chars().next().unwrap();
+
+        if !letter.is_lowercase() || input != format!("{}\n", letter) {
+            println!("Please enter a single, lowercase letter as input");
+            continue;
+        }
+
+        let mut wrong = true;
+
+        let occurs : Vec<_> = word.match_indices(letter).map(|(i, _)|i).collect();
+        for i in occurs {
+            wrong = false;
+            count += 1;
+            corrects.replace_range((2 * i + 1)..(2 * i + 2), letter.to_string().as_str());
+        }
+
+        if count == word.len() {
+            println!("You Win!");
+            return;
+        }
+
+        if wrong {
+            guessed.push(' ');
+            guessed.push(letter);
+            match wrong_guesses {
+                0 => gallows.replace_range(12..13, "|"),
+                1 => gallows.replace_range(19..20, "O"),
+                2 => gallows.replace_range(27..28, "|"),
+                3 => gallows.replace_range(26..27, "/"),
+                4 => gallows.replace_range(28..29, "\\"),
+                5 => gallows.replace_range(34..35, "/"),
+                6 => gallows.replace_range(36..37, "\\"),
+                _ => println!("This shouldn't happen..."),
+            }
+            wrong_guesses += 1
+        }
+
+    }
 }
